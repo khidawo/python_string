f8324e1630fabb3e810e70c8cb57afa2ade96230
khidawo <77900099+khidawo@users.noreply.github.com>
2021-03-25 20:43:27 +0300

    Add files via upload

---
+++ python_string.py
@@ -0,0 +1,24 @@
+#multiline strings
+a = """you fit into me
+like a hook into an eye
+
+a fish hook
+an open eye"""
+print(a) 
+#looping through a string
+for x in "banana":
+  print(x)
+#use len() to get length of a string
+a = "Hello, World!"
+print(len(a))
+
+#string slicing
+name="strongwarriors"
+print("initial string:")
+print(name)
+
+#printing 3rd to 12th character
+print("\n slicing from 3-12:")
+print(name[3:12])
+
+
